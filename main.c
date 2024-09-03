04ce8ed8c840c15d46c71bfa746407599dfe296e
KianRistori <ristorikian@gmail.com>
2024-09-03 19:35:58 +0200

    Add libft function to malloc

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include "malloc.h"
 
 int main()
 {
-	printf("Hello world\n");
+	char* str = "Hello";
+	int len = ft_strlen(str);
+	printf("len: %d\n", len);
 	return 0;
 }
---
+++ malloc.h
@@ -0,0 +1,6 @@
+#ifndef MALLOC_H
+# define MALLOC_H
+
+# include "libft/libft.h"
+
+#endif
